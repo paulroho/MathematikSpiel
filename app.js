78d49f6dc57e34c15962d2bbd84f081aec304cf3
Elias Wieser <eliaswieser@Paulis-MacBook-Pro.local>
2017-10-08 15:05:05 +0200

    Zufallszahlen erzeugen und aufräumen

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 (function(doc){
-
+    
+    var zahl1 = doc.getElementById("zahl1");
+    var zahl2 = doc.getElementById("zahl2");
     var fertigButton = doc.getElementById("fertigKnopf");
+    var eingegeben = doc.getElementById("ergebnis");
+    
+    zahl1.innerText = getRandomInt(100, 8000);    
+    zahl2.innerText = -getRandomInt(100, 8000);
+
     fertigButton.addEventListener("click", function() {
-        var eingegeben = doc.getElementById("ergebnis");
         alert("Es wurde '" + eingegeben.value + "' eingegeben.");
-   
     });
 
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 })(document);
